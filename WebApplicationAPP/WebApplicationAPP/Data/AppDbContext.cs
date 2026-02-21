0b9a62957454177ea2690da25570cc5e8a96aa7d
SusoGtx <153784087+SusoGtx@users.noreply.github.com>
2026-02-20 18:52:13 -0600

    Push Profe
    Proyecto Actual del Profe

---
+++ WebApplicationAPP/WebApplicationAPP/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationAPP.Models;
+
+namespace WebApplicationAPP.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        //Model de datos de persona en la tabla de mysql
+        public DbSet<Persona> Persona { get; set; }
+        
+    }
+}
